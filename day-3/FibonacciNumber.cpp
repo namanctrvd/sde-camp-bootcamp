ae32a55ab648405aa5713b1f0bef449ee7b8573c
Naman <naman19102@iiitnr.edu.in>
2022-02-09 01:03:42 +0530

    minor upadate

--- day-3/FibonacciNumber.cpp
+++ day-3/FibonacciNumber.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+int fib(int);
+
+int main() {
+    int n, result;
+    cout << "which fibonacci number you want?" <<endl;
+    cin >> n;
+    result = fib(n);
+    cout << n << "th fibonacci number is: " << result << endl;
+}
 
 int fib(int a) {
     if((a==1)||(a==0)){
@@ -8,13 +17,4 @@ int fib(int a) {
     else {
         return (fib(a-1) + fib(a-2));
     }
-}
-
-int main() {
-    int n, result;
-    cout << "which fibonacci number you want?" <<endl;
-    cin >> n;
-    result = fib(n);
-    cout << n << "th fibonacci number is: " << result << endl;
-
 }
